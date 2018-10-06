53e4e126181751aa731f7f6ea567f98383ff2518
Rohnit Shetty <rshetty6@stevens.edu>
2018-10-06 16:10:28 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# RedditImageCrawler
+# RedditImageCrawler
---
+++ app.py
@@ -0,0 +1,45 @@
+#! usr/bin/env python3
+import praw
+import pandas as pd
+import datetime as dt
+import os
+from urllib.error import URLError, HTTPError
+import urllib.request as web
+import shutil
+
+import colorama
+from colorama import Fore, Style
+
+reddit = praw.Reddit(client_id='4_449rzO0nHMYg', \
+                     client_secret='3W-qwMbWzkIecvoOU9yIuGGRJ-0', \
+                     user_agent='wallpaperScraper', \
+                     username='rohnit15', \
+                     password='rohnit15')
+
+
+
+
+current_dir = os.getcwd()
+dir_path = os.path.join(current_dir, "earthporn")
+if not os.path.exists(dir_path):
+    os.mkdir(dir_path)
+
+
+colorama.init(autoreset= True)
+subreddit = reddit.subreddit("earthporn")
+
+print("Downloading Files...")
+
+
+top_sub = subreddit.top(limit=10)
+
+for submission in top_sub:
+    fullfilename = os.path.join(dir_path, "{}.jpg".format(submission))
+   
+    request = web.Request(submission.url, headers = {'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'})
+    with web.urlopen(request) as response, open(fullfilename, 'wb') as out_file:
+        shutil.copyfileobj(response, out_file)
+        
+
+
+    
